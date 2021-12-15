f7b973966376f62ff08c581f7c092ac304a21385
Joshua Morales <jamorales121@gmail.com>
2021-12-15 12:33:09 -0500

    Set up database and made Category Model

---
+++ WebMVCApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebMVCApp.Models;
+
+namespace WebMVCApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) // Required for all database interactions
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; } // Sets Table name in database for the Category model
+    }
+}
